fff8c28ea1318dc410aba6abee588bc14f658350
Stephen Nicholas <stephennicholas.191200@gmail.com>
2020-11-29 20:00:00 +0700

    Added new function for firebase template

--- app/src/main/java/com/example/simpleapp/LoginActivity.java
+++ app/src/main/java/com/example/simpleapp/LoginActivity.java
@@ -1,14 +1,40 @@
 package com.example.simpleapp;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
+    private void updateUI(FirebaseUser currentUser) {
+        Context context = getApplicationContext();
+        Toast.makeText(context, "User Login: "+currentUser.getDisplayName(),Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button register = findViewById(R.id.buttonRegister);
+
+        register.setOnClickListener(v -> {
+            Context context = getApplicationContext();
+            String name = ((EditText) findViewById(R.id.nameRegister)).getText().toString();
+            String email = ((EditText) findViewById(R.id.emailRegister)).getText().toString();
+            String password = ((EditText) findViewById(R.id.passwordRegister)).getText().toString();
+            String confirmationPassword = ((EditText) findViewById(R.id.confPasswordRegister)).getText().toString();
+            Toast.makeText(context, "Not Connect to Firebase Yet. But the email is: " + email + " and the password is: " + password,Toast.LENGTH_SHORT).show();
+        });
     }
 }
--- app/src/main/java/com/example/simpleapp/RegisterActivity.java
+++ app/src/main/java/com/example/simpleapp/RegisterActivity.java
@@ -2,7 +2,11 @@ package com.example.simpleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button login = findViewById(R.id.buttonLogin);
+
+        login.setOnClickListener(v -> {
+            Context context = getApplicationContext();
+            String email = ((EditText) findViewById(R.id.emailLogin)).getText().toString();
+            String password = ((EditText) findViewById(R.id.passwordLogin)).getText().toString();
+            Toast.makeText(context, "Not Connect to Firebase Yet. But the email is: " + email + " and the password is: " + password,Toast.LENGTH_SHORT).show();
+        });
     }
 }
